7ebbd8899e4221c8a3d001cea87a7d2f91ece3fe
dead8309 <68665948+dead8309@users.noreply.github.com>
2020-11-12 18:10:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config(object):
-	BOT_TOKEN = os.environ.get('BOT_TOKEN', '1476773425:AAH3XVG13N7P2v__IKmo8JrJbfcNIGef20Q')
-	GUSERNAME = os.environ.get('GUSER_NAME', 'deadyt8309@gmail.com')
-	GPASSWORD = os.environ.get('GPASSWORD', '+919835495101')
+	BOT_TOKEN = os.environ.get('BOT_TOKEN')
+	GUSERNAME = os.environ.get('GUSER_NAME')
+	GPASSWORD = os.environ.get('GPASSWORD')
 
 # If you're not familiar with how to set Environment Variables you can do like this instead
 # of  setting Environment Variables
